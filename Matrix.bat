2c4e37a7aa07d05f4f5c139158518068dbe150cd
Project-eSeL <51643747+Project-eSeL@users.noreply.github.com>
2019-06-10 14:45:05 +0200

    Create Matrix.bat

---
+++ Matrix.bat
@@ -0,0 +1,9 @@
+@echo off
+title matrix
+color 0a
+mode 1000
+goto Matrix
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
